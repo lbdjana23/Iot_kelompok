795bf157bc908912afcdd2a262f9b928933e1415
lbdjana23 <ngurahlabda@gmail.com>
2021-05-17 00:16:56 +0800

    menambahkan

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 const char* WIFI_SSID = "Pipit dewi";
 const char* WIFI_PASS = "Omas5758";
-const char* HOSTNAME = "labdajana;";
+const char* HOSTNAME = "labdajana:";
 
 OneWire oneWire(4);
 DallasTemperature sensors(&oneWire);
